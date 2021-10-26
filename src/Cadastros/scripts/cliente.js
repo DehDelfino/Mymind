fae7ba9f182eae6d392e949b9bcf7169c7d64beb
Andre <andredelphin@outlook.com>
2021-10-26 11:58:58 -0300

    criando validação

--- src/Cadastros/scripts/cliente.js
+++ src/Cadastros/scripts/cliente.js
@@ -1,47 +1,98 @@
-const usuariosA = []
+
 const novoUsuario = []
 
 
-function cadastrar(){
-  //dados
-  
 
-  const $nomeCompletoValue = document.getElementById("nomeCompleto").value
-  const $dataNascimentoValue = document.getElementById("dtnasc").value
-  const $sexoValue = document.getElementById("sexo").value
-  // const $sexoValue = document.querySelector("input[name='sexo']:checked").value
-  const $telefoneValue = document.getElementById("telefone").value
-  const $emailValue = document.getElementById("email").value
-  const $cpfValue = document.getElementById("cpf").value
-  const $tempoValue = document.getElementById("tempo")[$tempoValue.selectedIndex].text
-  const $comoNosConheceuValue = document.getElementById(" comoNosConheceu")[$comoNosConheceuValue.selectedIndex].text
-  const $userValue = document.getElementById("user")[$userValue.selectedIndex].text
- 
- 
- 
+function validacaoSenha ( senha, confSenha){
 
-  const usuario = {
-    nome:$nomeCompletoValue,
-    data:$dataNascimentoValue,
-    // sexo:$sexoValue,
-    telefone:$telefoneValue,
-    email: $emailValue,
-    cpf: $cpfValue,
-    tempo: $tempoValue,
-    ComoNosConheceu: $comoNosConheceuValue,
-    user = $userValue,
+
+
+  if (senha === confSenha){
+    
+    const dadosUsuario =
+    {
+      nome:$nomeCompletoValue,
+      data:$dataNascimentoValue,
+    
+      telefone:$telefoneValue,
+      email: $emailValue,
+      cpf: $cpfValue,
+      acompanhamento_psicológico: $tempoValue,
+      ComoNosConheceu: $comoNosConheceuValue,
+      user: $userValue,
+      password: $senha, 
+
+    }
+
+    novoUsuario.push(dadosUsuario)
+
+    const usuariosA = JSON.parse(localStorage.getItem("usuarios"))
+    
+
+    const usuarios = [...usuariosA, ...novoUsuario]
+    
+    
+
+    window.localStorage.setItem("usuarios",JSON.stringify(usuarios))
+
+    return true
+
 
   }
 
+  else{
+    return false
+  }
+}
+
+function validacaoUsuario (usuario){
+  const usuariosA = JSON.parse(localStorage.getItem("usuarios"))
+
+  usuariosA.map((usuario)=> usuario===usuario.user)
+
+  if(usuariosA){
+    return false
+  }
+
+}
+
+
+
+
+
+function cadastrar(){
+
+  //dados
+  const $nomeCompletoValue = document.getElementById("nomeCompleto").value
+  const $dataNascimentoValue = document.getElementById("dtnasc").value
+ 
+
+  const $telefoneValue = document.getElementById("telefone").value
+  const $emailValue = document.getElementById("email").value
+  const $cpfValue = document.getElementById("cpf").value
+
+  const $tempo = document.getElementById("tempo")
+  const $tempoValue = $tempo.options[$tempo.selectedIndex].text
+
+  const $comoNosConheceu = document.getElementById("comoNosConheceu")
+
+  const $comoNosConheceuValue = $comoNosConheceu.options[$comoNosConheceu.selectedIndex].text
+  const $userValue = document.getElementById("user").value
+
+  const $senha = document.getElementById("senha").value
+  const $senhaConfirmacao = document.getElementById("confSenha").value
+
+
+ const validacaoSenha = validacaoSenha ( $senha, $senhaConfirmacao)
+
+
+  
+  
   
 
 
-  novoUsuario.push(usuario)
-
-  const usuarios = [...usuariosA, ...novoUsuario]
-  
-
- window.localStorage.setItem("usuarios",JSON.stringify(usuarios))
+ 
+ 
 
 
   
