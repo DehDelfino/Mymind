aafc90119976e26d9c5b4b64f1b17e8d9be68f59
Andre <andredelphin@outlook.com>
2021-10-27 22:29:40 -0300

    Co-authored-by: Giulia  <GiuliaCarvalhoS@users.noreply.github.com> Co-authored-by: Fernando Dias <FernandoDias18@users.noreply.github.com>

--- src/externo/artigos/js/script.js
+++ src/externo/artigos/js/script.js
@@ -1,15 +1,55 @@
-var listaArtigos = [[0, 'titulo 1', 'texto1', 'autor1' ][0, 'titulo 1', 'texto1', 'autor1' ]]
-window.onload = function() {
-    montarCardProduto();
-}
+const listaArtigos = JSON.parse( localStorage.getItem("artigo"))
+
+console.log(listaArtigos)
 
 function montarCardProduto() {
-    for(var i=0; i<listaArtigos.length; i++)
-    {
-        var conteudo = "";
-        conteudo += <div class ="">;
-        conteudo += listaArtigos[i][1];
-        conteudo +=</div>;
-        document.getElementById('div-pagina').innerHTML += conteudo;
-    }
+
+    const div = document.getElementById('div-pagina')
+
+    listaArtigos.map(({autor, texto, titulo})=>{
+        
+        
+
+        let conteudo = "";
+        conteudo += "<div class='mldssocorro'"
+            conteudo += '<br>'
+            conteudo += ` <h2> titulo: ${titulo} </h2>` ;
+            conteudo += '<br>'
+            conteudo += '<br>'
+            conteudo += '<h3>'
+            conteudo += ` autor: ${autor}`
+            conteudo += '</h3>'
+            conteudo += '<br>'
+            conteudo += '<br>'
+            conteudo += `<p> ${texto} </p>`
+        conteudo += "</div>"
+
+
+        console.log(conteudo)
+        if(conteudo.type ===undefined){
+            div.innerHTML += conteudo
+        }
+        
+        
+      ;
+    })
+    
+
+
+
+
+
+
+
+    // for(var i=0; i<listaArtigos.length; i++)
+    // {
+    //     console.log(listaArtigos)
+    //     var conteudo = "";
+    //     conteudo += "<div>";
+    //     conteudo += `${listaArtigos[i]}`;
+    //     conteudo += "</div>";
+    //     document.getElementById('div-pagina').innerHTML += conteudo;
+    // }
 }
+
+montarCardProduto()
